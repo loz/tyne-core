7638dd1f5bc54cdecea81e9e6e62afda920dd2e7
Michael Leßnau <michael.lessnau@googlemail.com>
2012-06-06 18:38:20 +0200

    setup rspec

--- lib/tyne-core/engine.rb
+++ lib/tyne-core/engine.rb
@@ -1,5 +1,10 @@
 module TyneCore
   class Engine < ::Rails::Engine
     isolate_namespace TyneCore
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
