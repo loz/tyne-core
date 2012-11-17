c3fdecb2df476d85e75887e7b221662b7a6c7b21
Tobias Haar <tobias.haar@googlemail.com>
2012-11-17 22:58:20 +0000

    applies a regex format validator to keys

--- app/models/tyne_core/project.rb
+++ app/models/tyne_core/project.rb
@@ -5,6 +5,7 @@ module TyneCore
 
     validates :key, :name, :user_id, :presence => true
     validates :key, :name, :uniqueness => { :scope => :user_id }
+    validates :key, :format => { :with => /^[a-zA-Z\d\s]*$/ }
 
     belongs_to :user, :class_name => "TyneAuth::User"
     has_many :issues, :class_name => "TyneCore::Issue"
