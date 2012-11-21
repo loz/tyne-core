1fe81d810e0e747ea8e25eacd512a43709e64733
Tobias Haar <tobias.haar@googlemail.com>
2012-11-21 20:52:29 +0000

    adds documentation and specs for issue cell

--- app/cells/issue_cell.rb
+++ app/cells/issue_cell.rb
@@ -1,4 +1,8 @@
+# Represents a create button for issues
 class IssueCell < Cell::Rails
+  # Renders a new create button for issue creation
+  #
+  # @param [String] dialog_path PJAX path to the dialog
   def create(dialog_path)
     @dialog_path = dialog_path
     render
