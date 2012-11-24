c7fe6311cbd679519740dd7dbc19e84f26b565a8
Tobias Haar <tobias.haar@googlemail.com>
2012-11-24 00:54:56 +0000

    updates spec helper to use random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,9 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.order = "random"
+
   config.include TyneAuth::Engine.routes.url_helpers
   config.include TyneCore::Engine.routes.url_helpers
   config.include Rails.application.routes.url_helpers
