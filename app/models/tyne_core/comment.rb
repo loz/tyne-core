f01e8690fe03c8cccbed1751f335339619b65873
Tobias Haar <tobias.haar@googlemail.com>
2012-11-24 01:09:56 +0000

    touch issue when comment is saved

--- app/models/tyne_core/comment.rb
+++ app/models/tyne_core/comment.rb
@@ -2,7 +2,7 @@ module TyneCore
   # A comment is a message on a issue
   class Comment < ActiveRecord::Base
     belongs_to :user, :class_name => "TyneAuth::User"
-    belongs_to :issue, :class_name => "TyneCore::Issue"
+    belongs_to :issue, :class_name => "TyneCore::Issue", :touch => true
 
     validates :message, :issue_id, :user_id, :presence => true
 
