77456008608223788042bafa3188203f3f37ed22
Tobias Haar <tobias.haar@googlemail.com>
2012-11-24 23:08:06 +0000

    enhancements for comment section

--- vendor/assets/javascripts/cells/comment.js
+++ vendor/assets/javascripts/cells/comment.js
@@ -15,9 +15,14 @@
       success: function(responseText, status, xhr) {
         _this.$target.parent().find(".comments").append(responseText);
         _this.$target.populate({});
+        _this.$target.find('input[name="commit"]').removeAttr("disabled");
         Notification.show('success', I18n.t("flash.actions.create.notice", {resource_name: "Comment"}));
       }
     };
+
+    _this.$target.on("submit", function() {
+      _this.$target.find('input[name="commit"]').attr("disabled", "disabled");
+    });
 
     _this.$target.ajaxForm(options);
   };
