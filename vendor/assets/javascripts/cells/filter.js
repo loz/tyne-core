0cbc5601f15259aafa5bcf720e56b2c5dd8e4a22
Chris Barber <chris.barber@sage.com>
2012-11-27 00:16:35 +0000

    Tidy JS of Filter

--- vendor/assets/javascripts/cells/filter.js
+++ vendor/assets/javascripts/cells/filter.js
@@ -5,54 +5,54 @@
   Filter.prototype.initialize = function() {
     var _this = this;
 
-    _this.form = _this.$target.find("form");
+    this.form = _this.$target.find("form");
 
-    _this.$target.on("click", "label", function() {
-      event.preventDefault();
+    this.$target.on("click", "label", function(ev) {
+      ev.preventDefault();
 
-      var _label = $(event.target);
-      var _filter = _label.closest(".filter");
-      var _option = _label.closest(".filter-options");
-      var _input = _option.find("input");
-      var _link = _option.find("a");
+      var label = $(ev.target),
+          filter = label.closest(".filter"),
+          option = label.closest(".filter-options"),
+          input = option.find("input"),
+          link = option.find("a");
 
-      _filter.find(".icon-remove").removeClass("icon-remove").addClass("icon-plus");
-      _filter.find(":input").not(_input).removeAttr("checked");
-      _filter.find(".selected").removeClass("selected");
-      _input.attr("checked", "checked");
+      filter.find(".icon-remove").removeClass("icon-remove").addClass("icon-plus");
+      filter.find(":input").not(input).removeAttr("checked");
+      filter.find(".selected").removeClass("selected");
+      input.attr("checked", "checked");
 
-      _option.addClass("selected");
-      _link.removeClass("icon-plus").addClass("icon-remove");
+      option.addClass("selected");
+      link.removeClass("icon-plus").addClass("icon-remove");
 
       Backlog.instances[0].refresh(true);
     });
 
-    _this.$target.on("click", ".filter-add", function() {
-      event.preventDefault();
+    this.$target.on("click", ".filter-add", function(ev) {
+      ev.preventDefault();
 
-      var _target = $(event.target);
-      var _filter = _target.closest(".filter");
-      var _filterOption = _target.closest(".filter-options");
-      var _input = _filterOption.find("input");
-      var _all = _filter.find('[id*=_all]').closest(".filter-options");
-      var _selected = _filterOption.is(".selected");
+      var target = $(ev.target),
+          filter = target.closest(".filter"),
+          filterOption = target.closest(".filter-options"),
+          input = filterOption.find("input"),
+          all = filter.find('[id*=_all]').closest(".filter-options"),
+          selected = filterOption.is(".selected");
 
-      if (_selected) {
+      if (selected) {
         // Remove filter
-        _input.removeAttr("checked");
-        _target.removeClass("icon-remove").addClass("icon-plus");
-        _filterOption.removeClass("selected");
+        input.removeAttr("checked");
+        target.removeClass("icon-remove").addClass("icon-plus");
+        filterOption.removeClass("selected");
 
         // Select all if no filter is applied
-        if (_filter.find('input:checked').length == 0) {
-          _all.addClass("selected");
+        if (filter.find('input:checked').length == 0) {
+          all.addClass("selected");
         }
       } else {
         // Add filter
-        _all.removeClass("selected");
-        _input.attr("checked", "checked");
-        _target.removeClass("icon-plus").addClass("icon-remove");
-        _filterOption.addClass("selected");
+        all.removeClass("selected");
+        input.attr("checked", "checked");
+        target.removeClass("icon-plus").addClass("icon-remove");
+        filterOption.addClass("selected");
       };
 
       Backlog.instances[0].refresh(true);
@@ -60,39 +60,40 @@
   };
 
   Filter.prototype.resetState = function(data) {
-    var _this = this;
-    var prefix = "filter";
-    var _filter = _this.$target.find(".filter");
+    var _this = this,
+        prefix = "filter",
+        filter = _this.$target.find(".filter");
 
     // Set values
-    _filter.each(function(index, element) {
-      var _target = $(element);
-      _target.find(".selected").removeClass("selected");
-      _target.find(".icon-remove").addClass("icon-plus");
-      _target.find(":checked").removeAttr("checked");
-      var field = _target.data('filter');
+    filter.each(function(index, element) {
+      var target = $(element),
+          field = target.data('filter');
+
+      target.find(".selected").removeClass("selected");
+      target.find(".icon-remove").addClass("icon-plus");
+      target.find(":checked").removeAttr("checked");
 
       if (data.filter && data.filter[field]) {
         // Select entry
         var value = data.filter[field];
 
         $.each(value, function(innerIndex, innerElement) {
-          var identifier = prefix + "_" + field + "_" + innerElement;
-          var element = _target.find("#" + identifier);
+          var identifier = prefix + "_" + field + "_" + innerElement,
+              element = target.find("#" + identifier);
           element.attr("checked", "checked");
           element.closest(".filter-options").addClass("selected");
           element.closest(".filter-options").find("a").removeClass("icon-plus").addClass("icon-remove");
         });
       } else {
         // Select all
-        _target.find('input[id*="_all"]').closest(".filter-options").addClass("selected");
+        target.find('input[id*="_all"]').closest(".filter-options").addClass("selected");
       }
     });
   };
 
   Filter.prototype.options = function() {
     var _this = this;
-    var filter = _this.form.formParams()["filter"];
+        filter = _this.form.formParams()["filter"];
     for (var k in filter) {
       if (!filter[k]) delete filter[k];
     }
