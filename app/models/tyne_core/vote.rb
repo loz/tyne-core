d2e04edccd00144a0c2c607824ae816ab761c456
Tobias Haar <tobias.haar@googlemail.com>
2012-12-10 19:34:38 +0000

    touch parent object when voted

--- app/models/tyne_core/vote.rb
+++ app/models/tyne_core/vote.rb
@@ -2,7 +2,7 @@ module TyneCore
   # Represents a vote (up/down) for an arbitrary resource
   class Vote < ActiveRecord::Base
     belongs_to :user, :class_name => '::TyneAuth::User'
-    belongs_to :votable, :polymorphic => true
+    belongs_to :votable, :polymorphic => true, :touch => true
 
     attr_accessible :user, :weight
 
