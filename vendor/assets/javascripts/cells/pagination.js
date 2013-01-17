4f65ba978970d7967c3b208394717b0516b78255
Tobias Haar <tobias.haar@googlemail.com>
2013-01-17 21:22:12 +0000

    tweaks pagination styling

--- vendor/assets/javascripts/cells/pagination.js
+++ vendor/assets/javascripts/cells/pagination.js
@@ -6,14 +6,23 @@
     var _this = this;
 
     _this.page = _this.$target.data("page");
+    _this.max = _this.$target.data("max");
     _this.form = _this.$target.find("form");
 
-    _this.$target.on("click", '[data-pagination="prev"]', function() {
+    _this.$target.on("click", '[data-pagination="prev"]', function(event) {
+      if (_this.page == 1) {
+        event.preventDefault();
+        return;
+      }
       _this.page--;
       Backlog.instances[0].refresh(true);
     });
 
-    _this.$target.on("click", '[data-pagination="next"]', function() {
+    _this.$target.on("click", '[data-pagination="next"]', function(event) {
+      if (_this.page == _this.max) {
+        event.preventDefault();
+        return;
+      }
       _this.page++;
       Backlog.instances[0].refresh(true);
     });
@@ -31,7 +40,6 @@
     var _this = this;
     var pagination = _this.form.formParams()["pagination"];
     pagination.page = _this.page;
-    console.log("page", pagination.page);
 
     return pagination;
   };
